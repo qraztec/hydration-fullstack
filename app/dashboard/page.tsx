ccebb059cbf6375acf17b0b92e1380cd8d8cbd7d
Gerum Haile-Lincoln <ghaileli@asu.edu>
2024-10-19 20:24:13 -0700

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,18 +3,18 @@
 import Navbar from '../components/Navbar';
 import StationMonitor from '../components/StationMonitor';
 import { useStationContext } from '../../context/StationContext';
-
+//Objective: display a dashboard based on current selected station from the user
 function Dashboard() {
   const { selectedStation, setSelectedStation } = useStationContext();
-
+  //Function to set the station to the one selected by user
   const handleStationSelect = (station) => {
     setSelectedStation(station);
   }
-
+  //Show loading if there is an error where no station is selected
   if (!selectedStation) {
     return <div>Loading stations...</div>; // Show loading until a station is selected
   }
-
+  //Navigation bar on top with station monitor component showing selectedstation data
   return (
     <div>
       <Navbar onSelectStation={handleStationSelect} selectedStation={selectedStation} />
